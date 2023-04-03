600c0922f8d7bf81db50783b085f4a7761633213
Marcus Stephan <cottageengtwo@mms-MacBook-Pro.local>
2023-04-03 10:24:48 -0700

    Coding Block 1 The columns skeleton

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <div className="board">
+          <div className="leftField"></div>
+          <div className="spaceCol"></div>
+          <div className="playField"></div>
+          <div className="spaceCol"></div>
+        </div>
+      </header>
     </div>
   );
 }
